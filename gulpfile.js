51de15c36d24b2b729b24c60ccbf941084748a3e
Juan Carlos Ruiz <juan.carlos.ruiz@janium.com>
2015-02-16 18:01:10 -0600

    Actualización de plugin Bootstrap switch
    Se actualizó a versión 3.3.2 de plugin, la versión anterior era la
    2.0.1.

--- gulpfile.js
+++ gulpfile.js
@@ -44,6 +44,7 @@ gulp.task('bootstrap-fonts', function() {
 gulp.task('build-less', function () {
 
   return gulp.src(config.JNMPath + '/less/*.less')
+  //return gulp.src(config.JNMPath + '/less/deep-blue.less')
     // Compile LESS files
     .pipe(less().on('error', console.log))
 
