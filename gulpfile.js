c795c2373430e467baca3e408a08f66c51403a8e
Juan Carlos Ruiz <juan.carlos.ruiz@janium.com>
2015-08-25 21:48:06 -0500

    Construyendo janium/htdocs
    Se agrega tema AdminLTE, actualización de Bootstrap y jQuery. Se
    prepara código para dejar de soportar IE8

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 // gulpfile.js
 
-// --- INIT
+//////// --- INIT
 var gulp    = require('gulp');
 var less    = require('gulp-less');
 var path    = require('path');
@@ -12,29 +12,62 @@ var clean   = require('del');
 var autoprefixer = require('gulp-autoprefixer');
 
 var config = {
+  MTRPath: path.join(__dirname, 'master_less'),
   JNMPath: path.join(__dirname, 'janium_components'),
-  FAPath: path.join(__dirname, 'bower_components', 'fontawesome'),
-  TBPath: path.join(__dirname, 'bower_components', 'bootstrap'),
+  BSPath:  path.join(__dirname, 'bower_components', 'bootstrap'),
+  LTEPath: path.join(__dirname, 'bower_components', 'AdminLTE'),
+  FAPath:  path.join(__dirname, 'bower_components', 'font-awesome'),
+  JQPath:  path.join(__dirname, 'bower_components', 'jquery'),
   JQCookiePath: path.join(__dirname, 'bower_components', 'jquery-cookie'),
-  JQJsonpPath: path.join(__dirname, 'bower_components', 'jquery-jsonp'),
-  JQPhPath: path.join(__dirname, 'bower_components', 'jquery-placeholder'),
-  JQValPath: path.join(__dirname, 'bower_components', 'jquery-validation'),
+  JQJsonpPath:  path.join(__dirname, 'bower_components', 'jquery-jsonp'),
+  JQPhPath:     path.join(__dirname, 'bower_components', 'jquery-placeholder'),
+  JQValPath:    path.join(__dirname, 'bower_components', 'jquery-validation'),
   //JQPrintPath: path.join(__dirname, 'bower_components', 'print-area'),
   //JQRespondPath: path.join(__dirname, 'bower_components', 'respond-minmax'),
   //JQZtreePath: path.join(__dirname, 'bower_components', 'ztree_v3'),
+  FIPath: path.join(__dirname, 'vendor_components', 'flaticon'),
   PubPath: path.join(__dirname, 'dist', 'htdocs')
 }
 
-// --- TASK
-// ** Copy fonts **//
+//////// --- TASK
+
+// ** Copy fonts ** //
+gulp.task('flaticon-fonts', function() {
+  return gulp.src(config.FIPath + '/fonts/**/*.{ttf,woff,eot,svg}')
+    .pipe(gulp.dest(path.join(config.PubPath, 'fonts')));
+});
+
 gulp.task('fontawesome-fonts', function() {
   return gulp.src(config.FAPath + '/fonts/**/*.{ttf,woff,eot,svg}')
     .pipe(gulp.dest(path.join(config.PubPath, 'fonts')));
 });
 
 gulp.task('bootstrap-fonts', function() {
-  return gulp.src(config.TBPath + '/fonts/**/*.{ttf,woff,eot,svg}')
+  return gulp.src(config.BSPath + '/fonts/**/*.{ttf,woff,eot,svg}')
     .pipe(gulp.dest(path.join(config.PubPath, 'fonts')));
+});
+
+// ** Compile LESS files ** //
+gulp.task('build-font-awesome', function () {
+  return gulp.src(config.MTRPath + '/font-awesome.less')
+    // Compile LESS files
+    .pipe(less().on('error', console.log))
+
+    // CSS Linter
+    .pipe(csslint('./janium_components/less/janium-theme/.csslintrc.json'))
+    .pipe(csslint.reporter())
+
+    // Create CSS file
+    .pipe(gulp.dest(path.join(config.PubPath, 'css', 'font-awesome')))
+
+    // Add .min to name file
+    .pipe(rename({suffix: '.min'}))
+
+    // Minify CSS file
+    .pipe(minify())
+
+    // Create minified file
+    .pipe(gulp.dest(path.join(config.PubPath, 'css', 'font-awesome')));
 });
 
 gulp.task('build-janium-theme', function () {
@@ -79,24 +112,26 @@ gulp.task('build-janium-theme', function () {
 
 });
 
-gulp.task('clean', function(cb) {
+
+//////// --- WATCH
+
+
+//////// --- TASK's
+
+gulp.task('clean-htdocs', function(cb) {
   // Clean destination folder
-  clean(['dist/htdocs/fonts/*', 'dist/htdocs/css/*'], cb)
-});
-
-// --- WATCH
-
-
-// --- TASK's
-gulp.task('clean-htdocs', ['clean'], function() {
   console.log('Limpiando directorio htdocs ...');
+  clean(['dist/htdocs/fonts/*', 'dist/htdocs/css/*', 'dist/htdocs/js/*'], cb)
 });
 
-gulp.task('copy-fonts', ['clean-htdocs', 'fontawesome-fonts', 'bootstrap-fonts'], function() {
-  console.log('Copiando fuentes FontAwesome y Bootstrap...');
+//gulp.task('clean-htdocs', ['clean'], function() {
+//  console.log('Copiando fuentes Flaticon, FontAwesome y Bootstrap...');
+//});
+
+gulp.task('copy-fonts', ['clean-htdocs', 'flaticon-fonts', 'fontawesome-fonts', 'bootstrap-fonts'], function() {
 });
 
-gulp.task('default', ['copy-fonts'], function() {
+gulp.task('default', ['copy-fonts', 'build-font-awesome'], function() {
   console.log('Compilando archivos LESS ...');
   gulp.start('build-janium-theme');
 });
